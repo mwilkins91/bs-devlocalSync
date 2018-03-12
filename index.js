816b188d4a5e7d82ddab05823201293af02c6325
wilkinm <mark.wilkins@uberflip.com>
2018-03-12 16:05:17 -0400

    update to account for cdn

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-const url = 'https://app.romern.dev.flyptech.com'; //'https://app.romern.dev.flyptech.com'
 const browserSync = require('browser-sync');
+const app = browserSync.create('app');
+const cdn = browserSync.create('cdn');
 
-browserSync({
-	proxy: {
-		target: url
-	},
-	serveStatic: ['.']
+const cdnPort = 8080;
+cdn.init({
+  proxy: {
+    target: 'https://app.cdntwrk-dev.com',
+  },
+  port: cdnPort,
+  ui: {
+    port: cdnPort + 1,
+  },
+});
+const cdnIp = cdn.instance.utils.devIp[0];
+const cdnUrl = `https://${cdnIp}:${cdnPort}`;
+app.init({
+  proxy: {
+    target: 'https://app.uberflip-dev.com/',
+  },
+  rewriteRules: [
+    {
+      match: /https:\/\/app\.cdntwrk-dev\.com/g,
+      fn: function(req, res, match) {
+        return cdnUrl;
+      },
+    },
+  ],
 });
